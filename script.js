ded6c453340a117c0535e4a153ca508640e3bf36
Ildar Sadykov <usongbird@gmail.com>
2019-07-22 20:30:05 +0200

    1

---
+++ script.js
@@ -0,0 +1,32 @@
+var a = 1;
+var b = 2;
+var k = 10 % 2;
+var c = k + k + k*5 + k + a*b;
+alert(c);
+
+for( var i = 0; i < 10; i++ ) {
+    c = c * 2 - 1;
+    console.log(c);
+}
+
+
+var bananus = document.getElementById('bananus');
+
+// setInterval(function() {
+//     bananus.style.transition = '2s';
+//     bananus.style.translateX = '100px';
+// }, 100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
