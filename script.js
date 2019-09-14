90251037d9f26e4bce2f5c3f03403ec8b32a4ef6
Ildar Sadykov <usongbird@gmail.com>
2019-09-14 18:58:16 +0200

    Круглые карточки

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var a = 'al;';
 
 var d = 'ss;';
 
-setTimeout(wow, 10000);
-// setInterval(wow, 500)
+// setTimeout(wow, 10000);
+// // setInterval(wow, 500)
 
-function wow() {
-    var tochno = 'точно ';
-    for(var i = 1; i < 10; i++) {
-        tochno = tochno + tochno;
-        console.log('Ты ' + tochno + ' хочешь закрыть окно?');
-    }
+// function wow() {
+//     var tochno = 'точно ';
+//     for(var i = 1; i < 10; i++) {
+//         tochno = tochno + tochno;
+//         console.log('Ты ' + tochno + ' хочешь закрыть окно?');
+//     }
     
-}
+// }
